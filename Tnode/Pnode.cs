4151c0c8e7fc239213af779ba52b7cda3a0bb8f2
kaizhao <kaizhao@ksu.edu>
2018-09-06 09:12:43 -0500

    Add project files.

---
+++ Tnode/Pnode.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tnode
+{
+    abstract class Pnode
+    {
+        public abstract double evaluate();
+    }
+
+    class ValueNode: Pnode
+    {
+        double value;
+        public override double evaluate()
+        {
+            return value;            
+        }
+
+    }
+
+    class OpNode : Pnode
+    {
+        char op;
+        Pnode left;
+        Pnode right;
+
+        public override double evaluate()
+        {
+            switch (op)
+            {
+                
+                case '+': return left.evaluate() + right.evaluate();
+                case '*': return left.evaluate() * right.evaluate();
+                default: return -1;
+            }
+        }
+    }
+    /// <summary>
+    /// /////////////////////////////////////////////////////////////
+    /// </summary>
+
+    abstract class OpAbNode: Pnode
+    {
+       
+       protected Pnode left;
+       protected Pnode right;
+    }
+
+    class AdditionNode: OpAbNode
+    {
+        public override double evaluate()
+        {
+          return left.evaluate() + right.evaluate();
+        }
+    }
+
+    class NultiNode : OpAbNode
+    {
+        public override double evaluate()
+        {
+            return left.evaluate() * right.evaluate();
+        }
+    }
+}
+}
---
+++ Tnode/Tnode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tnode
+{
+    class Tnode
+    {
+        double value;
+        char theOperator;
+
+        Tnode left;
+        Tnode right;
+
+        double evaluate()
+        {
+            switch (theOperator)
+            {
+                case '#': return value;
+                case '+': return left.evaluate() + right.evaluate();
+                case '*': return left.evaluate() * right.evaluate();
+                default: return -1;
+            }
+        }
+
+    }
+
+    
+}
